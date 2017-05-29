ea220d96fa41d06d262aec1176947426fdf9bc38
Manuel Schmalstieg <git.code@ms-studio.net>
2017-05-30 00:11:45 +0200

    correctif redirection

--- vlan-voila/index.php
+++ vlan-voila/index.php
@@ -2,6 +2,9 @@
 	// redirect to index
 	// using $_SERVER ? 
 	
-	header("Location: http://label-cave12:8888/");
+	// header("Location: http://label-cave12:8888/");
+	// header('Location: /');
+	header("Location: http://".$_SERVER['HTTP_HOST']."/");
+	exit;
 	
 ?>
