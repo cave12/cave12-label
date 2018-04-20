0787b85a4ace7e7678fa4c2b8c73d41ce98fa5e6
Manuel Schmalstieg <git.code@ms-studio.net>
2018-04-20 17:10:55 +0200

    ne pas versionner le dossier des images

--- francisco-meirino-la-plainte/index.php
+++ francisco-meirino-la-plainte/index.php
@@ -4,6 +4,7 @@
 	
 	// header("Location: http://label-cave12:8888/");
 	// header('Location: /');
-	include('francisco-meirino-la-plainte/index-root.php');
+	header("Location: http://".$_SERVER['HTTP_HOST']."/");
+	exit;
 	
 ?>
