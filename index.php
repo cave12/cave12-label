2b84821410cdebf132ba3b726b301a095d16b452
Manuel Schmalstieg <git.code@ms-studio.net>
2021-01-08 01:21:18 +0100

    moving content to index-root

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 	// header("Location: http://label-cave12:8888/");
 	// header('Location: /');
 	// include('francisco-meirino-la-plainte/index-root.php');
-	include('uruk/index-root.php');
+	include('mika-vainio-last-live/index-root.php');
 	
 ?>
